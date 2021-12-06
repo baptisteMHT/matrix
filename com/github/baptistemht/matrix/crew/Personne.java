56a9649ceb510739ac9788899b6d8bd717d4c728
SytHamMer <mathys.lebon1@gmail.com>
2021-12-06 10:27:49 +0100

    attributs personne

--- com/github/baptistemht/matrix/crew/Personne.java
+++ com/github/baptistemht/matrix/crew/Personne.java
@@ -1,5 +1,20 @@
 package com.github.baptistemht.matrix.crew;
 
 public class Personne {
+    //atributs
+    private String nom;
+    private boolean estHomme;
+    private int age;
+    private grade grade;
+    
+    //methods
+
+
+    public Personne(String nom, boolean estHomme, int age, grade grade){
+        this.nom = nom;
+        this.estHomme = estHomme;
+        this.age = age;
+        this.grade = grade;
+    }
     
 }
