db26dffe12a6e9b0c87f6589004c887abc3d6774
SytHamMer <mathys.lebon1@gmail.com>
2021-12-06 10:56:35 +0100

    Update Personne

---
+++ com/github/baptistemht/matrix/crew/Personnel.java
@@ -0,0 +1,14 @@
+package com.github.baptistemht.matrix.crew;
+
+import java.util.ArrayList;
+
+public class Personnel {
+    //attributs
+    ArrayList<Personne> Personnel = new ArrayList<Personne>();
+    //methods
+
+    public void addPersonnel(Personne p){
+        Personnel.add(p);
+    }
+    
+}
